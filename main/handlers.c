66a7386b997d3f613fa0dad4879013c58ddea7b1
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-08-14 18:44:56 +0700

    seperate left and right wheel control

--- main/handlers.c
+++ main/handlers.c
@@ -15,22 +15,43 @@ const httpd_uri_t root = {
     .user_ctx = NULL
 };
 
-const httpd_uri_t maju = {
-    .uri = "/maju",
+const httpd_uri_t kanan_maju = {
+    .uri = "/kanan-maju",
     .method = HTTP_GET,
     .handler = maju_handler,
     .user_ctx = NULL
 };
 
-const httpd_uri_t mundur = {
-    .uri = "/mundur",
+const httpd_uri_t kanan_mundur = {
+    .uri = "/kanan-mundur",
     .method = HTTP_GET,
     .handler = mundur_handler,
     .user_ctx = NULL
 };
 
-const httpd_uri_t stop = {
-    .uri = "/stop",
+const httpd_uri_t kanan_stop = {
+    .uri = "/kanan-stop",
+    .method = HTTP_GET,
+    .handler = stop_handler,
+    .user_ctx = NULL
+};
+
+const httpd_uri_t kiri_maju = {
+    .uri = "/kiri-maju",
+    .method = HTTP_GET,
+    .handler = maju_handler,
+    .user_ctx = NULL
+};
+
+const httpd_uri_t kiri_mundur = {
+    .uri = "/kiri-mundur",
+    .method = HTTP_GET,
+    .handler = mundur_handler,
+    .user_ctx = NULL
+};
+
+const httpd_uri_t kiri_stop = {
+    .uri = "/kiri-stop",
     .method = HTTP_GET,
     .handler = stop_handler,
     .user_ctx = NULL
@@ -44,40 +65,67 @@ esp_err_t root_handler(httpd_req_t *req) {
     return ESP_OK;
 }
 
-esp_err_t maju_handler(httpd_req_t *req) {
+esp_err_t kanan_maju_handler(httpd_req_t *req) {
     xSemaphoreTake(mutex, portMAX_DELAY);
     gpio_set_level(RC_PIN_BAN_KANAN_MUNDUR, 0);
-    gpio_set_level(RC_PIN_BAN_KIRI_MUNDUR, 0);
     gpio_set_level(RC_PIN_BAN_KANAN_MAJU, 1);
+
+    httpd_resp_send(req, "kanan maju", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send_chunk(req, NULL, 0);
+    xSemaphoreGive(mutex);
+    return ESP_OK;
+}
+
+esp_err_t kanan_mundur_handler(httpd_req_t *req) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    gpio_set_level(RC_PIN_BAN_KANAN_MAJU, 0);
+    gpio_set_level(RC_PIN_BAN_KANAN_MUNDUR, 1);
+
+    httpd_resp_send(req, "kanan mundur", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send_chunk(req, NULL, 0);
+    xSemaphoreGive(mutex);
+    return ESP_OK;
+}
+
+esp_err_t kanan_stop_handler(httpd_req_t *req) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    gpio_set_level(RC_PIN_BAN_KANAN_MAJU, 0);
+    gpio_set_level(RC_PIN_BAN_KANAN_MUNDUR, 0);
+
+    httpd_resp_send(req, "kanan stop", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send_chunk(req, NULL, 0);
+    xSemaphoreGive(mutex);
+    return ESP_OK;
+}
+
+esp_err_t kiri_maju_handler(httpd_req_t *req) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    gpio_set_level(RC_PIN_BAN_KIRI_MUNDUR, 0);
     gpio_set_level(RC_PIN_BAN_KIRI_MAJU, 1);
 
-    httpd_resp_send(req, "maju", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send(req, "kiri maju", HTTPD_RESP_USE_STRLEN);
     httpd_resp_send_chunk(req, NULL, 0);
     xSemaphoreGive(mutex);
     return ESP_OK;
 }
 
-esp_err_t mundur_handler(httpd_req_t *req) {
+esp_err_t kiri_mundur_handler(httpd_req_t *req) {
     xSemaphoreTake(mutex, portMAX_DELAY);
-    gpio_set_level(RC_PIN_BAN_KANAN_MAJU, 0);
     gpio_set_level(RC_PIN_BAN_KIRI_MAJU, 0);
-    gpio_set_level(RC_PIN_BAN_KANAN_MUNDUR, 1);
     gpio_set_level(RC_PIN_BAN_KIRI_MUNDUR, 1);
 
-    httpd_resp_send(req, "mundur", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send(req, "kiri mundur", HTTPD_RESP_USE_STRLEN);
     httpd_resp_send_chunk(req, NULL, 0);
     xSemaphoreGive(mutex);
     return ESP_OK;
 }
 
-esp_err_t stop_handler(httpd_req_t *req) {
+esp_err_t kiri_stop_handler(httpd_req_t *req) {
     xSemaphoreTake(mutex, portMAX_DELAY);
-    gpio_set_level(RC_PIN_BAN_KANAN_MAJU, 0);
     gpio_set_level(RC_PIN_BAN_KIRI_MAJU, 0);
-    gpio_set_level(RC_PIN_BAN_KANAN_MUNDUR, 0);
     gpio_set_level(RC_PIN_BAN_KIRI_MUNDUR, 0);
 
-    httpd_resp_send(req, "stop", HTTPD_RESP_USE_STRLEN);
+    httpd_resp_send(req, "kiri stop", HTTPD_RESP_USE_STRLEN);
     httpd_resp_send_chunk(req, NULL, 0);
     xSemaphoreGive(mutex);
     return ESP_OK;
--- main/handlers.h
+++ main/handlers.h
@@ -8,11 +8,20 @@ extern SemaphoreHandle_t mutex;
 esp_err_t root_handler(httpd_req_t *req);
 extern const httpd_uri_t root;
 
-esp_err_t maju_handler(httpd_req_t *req);
-extern const httpd_uri_t maju;
+esp_err_t kanan_maju_handler(httpd_req_t *req);
+extern const httpd_uri_t kanan_maju;
 
-esp_err_t mundur_handler(httpd_req_t *req);
-extern const httpd_uri_t mundur;
+esp_err_t kanan_mundur_handler(httpd_req_t *req);
+extern const httpd_uri_t kanan_mundur;
 
-esp_err_t stop_handler(httpd_req_t *req);
-extern const httpd_uri_t stop;
+esp_err_t kanan_stop_handler(httpd_req_t *req);
+extern const httpd_uri_t kanan_stop;
+
+esp_err_t kiri_maju_handler(httpd_req_t *req);
+extern const httpd_uri_t kiri_maju;
+
+esp_err_t kiri_mundur_handler(httpd_req_t *req);
+extern const httpd_uri_t kiri_mundur;
+
+esp_err_t kiri_stop_handler(httpd_req_t *req);
+extern const httpd_uri_t kiri_stop;
