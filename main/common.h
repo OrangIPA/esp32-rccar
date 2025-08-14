b050522decd59e14915d77105566c9242ff13701
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-08-14 18:31:34 +0700

    change GPIO to drv pinout

--- main/common.h
+++ main/common.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define RC_PIN_BAN_KANAN_MAJU 1
-#define RC_PIN_BAN_KIRI_MAJU 2
-#define RC_PIN_BAN_KANAN_MUNDUR 3
-#define RC_PIN_BAN_KIRI_MUNDUR 4
+#define RC_PIN_BAN_KANAN_MAJU 3
+#define RC_PIN_BAN_KIRI_MAJU 4
+#define RC_PIN_BAN_KANAN_MUNDUR 5
+#define RC_PIN_BAN_KIRI_MUNDUR 2
 #define WIFI_SSID "esp32"
 #define WIFI_PASS "duato2two"
